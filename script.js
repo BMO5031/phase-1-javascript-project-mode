0bed94ff3495eaa29e83c7bb880b160c225d5c26
VALHALLA-GANGSTER <+>
2023-07-06 21:52:25 +0300

    retrieving weather data

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = "07f5de0e02514719823141045230607";
+  const APIKey = "6012dcbe60d44108ae3172021230607";
 
-    //function to fetch weather data
-function fetchWeatherData(city) {
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  // Function to fetch weather data
+  function fetchWeatherData(city) {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}`;
 
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        //process the weather data
+        // Process the weather data
         displayWeatherData(data);
       })
       .catch(error => {
         console.log('Error fetching weather data:', error);
       });
-}
+  }
 
-function displayWeatherData(data) {
+  function displayWeatherData(data) {
     const cityNameElement = document.getElementById('city-name');
     const temperatureElement = document.getElementById('temperature');
     const descriptionElement = document.getElementById('description');
@@ -26,22 +26,25 @@ function displayWeatherData(data) {
     temperatureElement.textContent = `Temperature: ${data.current.temp_c}°C`;
     descriptionElement.textContent = `Description: ${data.current.condition.text}`;
     humidityElement.textContent = `Humidity: ${data.current.humidity}%`;
-}
+  }
 
-//Add an event listener to the search buton
-const searchButton = document.getElementById('search-button');
-searchButton.addEventListener('click', function() {
-    const cityInput = document.getElementById('city-input');
-    const city = cityInput.Value;
-
-    fetchWeatherData(city);
-
-const form = document.querySelector('form');
-form.addEventListener('submit', function(event) {
+  // Add an event listener to the search button
+  const searchButton = document.getElementById('search-button');
+  searchButton.addEventListener('click', function() {
     const cityInput = document.getElementById('city-input');
     const city = cityInput.value;
 
     fetchWeatherData(city);
-})    
   });
-});
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    const cityInput = document.getElementById('city-input');
+    const city = cityInput.value;
+
+    fetchWeatherData(city);
+  });
+});  
+
